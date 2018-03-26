66781eecdaa5b2fc2de564eb964d0245f63665a1
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2018-03-25 22:38:22 -0500

    Move window, renderer related things into internal struct, add sdl image

--- main.c
+++ main.c
@@ -3,19 +3,39 @@
 
 #define SDL_MAIN_HANDLED
 #include "SDL.h"
+#include "SDL_image.h"
 
+#define SCREEN_TITLE "Seraph"
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+#define SCREEN_FLAGS SDL_WINDOW_RESIZABLE
+#define RENDER_FLAGS SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
 
 typedef struct Game {
     bool running;
-    SDL_Window *window;
-    SDL_Renderer *renderer;
-    SDL_Texture *texture;
+    struct {
+        const char *title;
+        unsigned int width;
+        unsigned int height;
+        unsigned int windowFlags;
+        unsigned int renderFlags;
+        SDL_Window *window;
+        SDL_Renderer *renderer;
+        SDL_Texture *texture;
+    } screen;
 } Game;
 Game game = {
         false,
-        NULL,
-        NULL,
-        NULL,
+        {
+                SCREEN_TITLE,
+                SCREEN_WIDTH,
+                SCREEN_HEIGHT,
+                SCREEN_FLAGS,
+                RENDER_FLAGS,
+                NULL,
+                NULL,
+                NULL,
+        }
 };
 
 void init() {
@@ -25,20 +45,27 @@ void init() {
         exit(1);
     }
 
-    SDL_CreateWindowAndRenderer(640, 480, SDL_WINDOW_RESIZABLE, &game.window, &game.renderer);
-    if (game.window == NULL) {
+    game.screen.window = SDL_CreateWindow(game.screen.title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                          game.screen.width, game.screen.height, game.screen.windowFlags);
+    if (game.screen.window == NULL) {
         SDL_Log("Failed to create window: %s", SDL_GetError());
         exit(1);
     }
 
-    SDL_Surface *surface = SDL_LoadBMP("data/test.bmp");
-    if (surface == NULL) {
-        SDL_Log("Failed to load bitmap: %s", SDL_GetError());
+    game.screen.renderer = SDL_CreateRenderer(game.screen.window, -1, game.screen.renderFlags);
+    if (game.screen.renderer == NULL) {
+        SDL_Log("Failed to create renderer: %s", SDL_GetError());
         exit(1);
     }
 
-    game.texture = SDL_CreateTextureFromSurface(game.renderer, surface);
-    if (game.texture == NULL) {
+    SDL_Surface *surface = IMG_Load("data/test.png");
+    if (surface == NULL) {
+        SDL_Log("Failed to load image: %s", IMG_GetError());
+        exit(1);
+    }
+
+    game.screen.texture = SDL_CreateTextureFromSurface(game.screen.renderer, surface);
+    if (game.screen.texture == NULL) {
         SDL_Log("Failed to create texture from surface: %s", SDL_GetError());
         exit(1);
     }
@@ -67,16 +94,16 @@ void update() {
 }
 
 void render() {
-    SDL_SetRenderDrawColor(game.renderer, 0x00, 0x00, 0x00, 0x00);
-    SDL_RenderClear(game.renderer);
-    SDL_RenderCopy(game.renderer, game.texture, NULL, NULL);
-    SDL_RenderPresent(game.renderer);
+    SDL_SetRenderDrawColor(game.screen.renderer, 0x00, 0x00, 0x00, 0x00);
+    SDL_RenderClear(game.screen.renderer);
+    SDL_RenderCopy(game.screen.renderer, game.screen.texture, NULL, NULL);
+    SDL_RenderPresent(game.screen.renderer);
 }
 
 void shutdown() {
-    SDL_DestroyTexture(game.texture);
-    SDL_DestroyRenderer(game.renderer);
-    SDL_DestroyWindow(game.window);
+    SDL_DestroyTexture(game.screen.texture);
+    SDL_DestroyRenderer(game.screen.renderer);
+    SDL_DestroyWindow(game.screen.window);
     SDL_Quit();
     game.running = false;
 }
